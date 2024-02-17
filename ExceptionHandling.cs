46c21893d4d870de366bd381220af1ac1aa1e714
Ni1ba <danila.baldin@mail.ru>
2024-02-17 20:01:30 +0300

    создал проект с тестами, просто потыкать че к чему

---
+++ ExceptionHandling.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+namespace TaskManagerConsole
+{
+    internal class ExceptionHandling
+    {
+        public int ValueNumeric(object value)
+        {
+            int result = 0;
+            try
+            {
+                value = int.TryParse(value.ToString(), out result);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally { }
+
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,11 +12,37 @@ namespace TaskManagerConsole
 
             Task task = new Task();
             //task.SetDateAdd(DateTime.Now);
-
             
+            Console.WriteLine("Hi");
+            Menu();
+
            
 
         }
+
+        public static void Menu()
+        {
+            ExceptionHandling exc = new ExceptionHandling();
+            int UserChoice;
+
+            do
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1.Список задач");
+                Console.WriteLine("2.Добавить задачу");
+                Console.WriteLine("3.Удалить задачу");
+                Console.WriteLine("4.Просмотреть задачу");
+
+                
+                 
+
+                UserChoice = exc.ValueNumeric( Console.ReadLine());
+
+            }
+            while (UserChoice!=0);
+        }
+
+        
     }
 
 }
